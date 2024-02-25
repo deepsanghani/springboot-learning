66e9d955d8e94c8ff612d5fdf5f5420571166d7b
Deep Sanghani <deepsanghani3@gmil.com>
2024-02-25 12:28:54 +0530

    learned mock

---
+++ src/test/java/com/example/demo/service/UserDetailsServiceTests.java
@@ -0,0 +1,39 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepo;
+import org.bson.assertions.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.mockito.*;
+
+import static org.mockito.Mockito.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+
+public class UserDetailsServiceTests {
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Mock
+    private UserRepo userRepo;
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void loadUserByUsernameTest(){
+        when(userRepo.findByUsername(ArgumentMatchers.anyString())).thenReturn(User.builder().username("Ram").password("abcd").roles(new ArrayList<>()).build());
+        UserDetails user = userDetailsService.loadUserByUsername("ram");
+        assertNotNull(user);
+    }
+}
