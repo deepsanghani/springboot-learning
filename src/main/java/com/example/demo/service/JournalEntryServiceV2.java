38ac7ec4b344327a41a6fef30779b012a97beb73
Deep Sanghani <deepsanghani3@gmil.com>
2024-02-29 17:33:33 +0530

    file changed

--- src/main/java/com/example/demo/service/JournalEntryServiceV2.java
+++ src/main/java/com/example/demo/service/JournalEntryServiceV2.java
@@ -54,7 +54,7 @@ public class JournalEntryServiceV2 {
                 journalEntryRepo.deleteById(id);
             }
         } catch (Exception e){
-            log.error("Error occured", e);
+            log.error("Error occured while", e);
             throw new RuntimeException("An error occured", e);
         }
         return removed;
