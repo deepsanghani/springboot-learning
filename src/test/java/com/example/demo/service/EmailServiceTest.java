14bf1e21aae6aaa7afc1fd7f10a8b8a5a0a49bf7
Deep Sanghani <deepsanghani3@gmil.com>
2024-03-13 22:13:53 +0530

    changes

--- src/test/java/com/example/demo/service/EmailServiceTest.java
+++ src/test/java/com/example/demo/service/EmailServiceTest.java
@@ -6,7 +6,6 @@ import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class EmailServiceTest {
 
     @InjectMocks
